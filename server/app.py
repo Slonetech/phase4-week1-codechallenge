3681c5b3575aab32d72994d8eb10239be59485a9
Slonetech <kengeregordon@gmail.com>
2023-09-27 12:11:28 +0300

    model completed

--- server/app.py
+++ server/app.py
@@ -1,8 +1,8 @@
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-from models import db
+from models import db, restaurant, restaurant_pizza, pizza
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -13,3 +13,52 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+@app.route('/')
+def index():
+    return 'welcome to best pizza'
+
+
+@app.route('/restaurants', methods=['GET'])
+def get_restaurants():
+    restaurants = restaurant.query.all()
+    return jsonify(restaurants)
+
+
+@app.route('/restaurants/<int:restaurant_id>', methods=['GET'])
+def get_restaurant(restaurant_id):
+    restaurant = restaurant.query.get(restaurant_id)
+    return jsonify(restaurant)
+
+
+@app.route('/restaurants/<int:restaurant_id>/pizzas', methods=['DELETE'])
+def delete_restaurant(restaurant_id):
+    restaurant = restaurant.query.get(restaurant_id)
+    db.session.delete(restaurant)
+    db.session.commit()
+    return make_response(jsonify({'message': 'restaurant deleted'}), 200)
+
+
+@app.route('/pizzas', methods=['GET'])
+def get_pizzas():
+    pizzas = Pizza.query.all()
+    pizza_list = [
+        {
+            'id': pizza.id,
+            'name': pizza.name,
+            'ingredients': pizza.ingredients
+        }
+        for pizza in pizzas
+    ]
+    return jsonify(pizza_list)
+
+
+
+@app.route('/restaurants_pizzas', methods=['POST'])
+def create_restaurant_pizza():
+    data = request.get_json()
+    pizza_id = data.get('pizza_id')
+    restaurant_id = data.get('restaurant_id')
+    price = data.get('price')
+
+    if not Pizza.query.get(pizza_id) or not restaurant_id:
+        return make_response(jsonify({'errors': ['Invalid pizza_id or restaurant_id']}), 400)
--- server/models.py
+++ server/models.py
@@ -1,40 +1,45 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import CheckConstraint, UniqueConstraint
 from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
-class restaurant(db.model,SerializerMixin):
+class Restaurant(db.Model, SerializerMixin):
     __tablename__ = 'restaurants'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
+    name = db.Column(db.String(50), nullable=False, unique=True)
     address = db.Column(db.String(120), nullable=False)
 
-    restaurant_pizzas = db.relationship('restaurant_pizza', backref='restaurant', lazy=True)
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref='restaurant', lazy=True)
+
+    # Add a UniqueConstraint for the name
+    __table_args__ = (
+        UniqueConstraint('name', name='unique_name_constraint'),
+    )
 
     def __repr__(self):
         return '<restaurant %r>' % self.name
-    
 
-class restaurant_pizza(db.model,SerializerMixin):
+class RestaurantPizza(db.Model):
     __tablename__ = 'restaurant_pizzas'
 
     id = db.Column(db.Integer, primary_key=True)
-    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'), nullable=False)
+    price = db.Column(db.Float, nullable=False)
     pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'), nullable=False)
-    price = db.Column(db.integer, nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False)
-    updated_at = db.Column(db.DateTime, nullable=False)
+    restaurant_id = db.Column(db.Integer, nullable=False)
 
-    restaurant = db.relationship('restaurant', backref='restaurant_pizzas', lazy=True)
-    pizza = db.relationship('pizza', backref='restaurant_pizzas', lazy=True)
+    pizza = db.relationship('Pizza', backref=db.backref('restaurant_pizzas', lazy=True))
 
+    # Add a CheckConstraint for the price
+    __table_args__ = (
+        CheckConstraint('price BETWEEN 1 AND 30', name='check_price_range'),
+    )
 
     def __repr__(self):
-        return '<restaurant_pizza %r>' % self.name
-    
+        return '<restaurant_pizza %r>' % self.price
 
-class pizza(db.model,SerializerMixin):
+class Pizza(db.Model, SerializerMixin):
     __tablename__ = 'pizzas'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -43,7 +48,7 @@ class pizza(db.model,SerializerMixin):
     created_at = db.Column(db.DateTime, nullable=False)
     updated_at = db.Column(db.DateTime, nullable=False)
 
-    restaurant_pizzas = db.relationship('restaurant_pizza', backref='pizza', lazy=True)
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref='pizza', lazy=True)
 
     def __repr__(self):
         return '<pizza %r>' % self.name
